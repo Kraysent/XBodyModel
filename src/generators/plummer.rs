ef46b262ffaf0c36d6933f506a7f5eff5d6683d3
Kraysent <kraysent@gmail.com>
2021-01-16 20:46:41 +0300

    fix: fixed Plummer sphere generation (velocities are still zero but at least positions are correct)

--- src/generators/plummer.rs
+++ src/generators/plummer.rs
@@ -8,13 +8,13 @@ use std::f64::consts::PI;
 pub struct Plummer
 {
     plummer_radius: f64,
-    n: u32,
+    n: usize,
     m0: f64,
 }
 
 impl Plummer
 {
-    pub fn new(plummer_radius: f64, n: u32, m0: Option<f64>) -> Result<Plummer, &'static str>
+    pub fn new(plummer_radius: f64, n: usize, m0: Option<f64>) -> Result<Plummer, &'static str>
     {
         if plummer_radius <= 0.0
         {
@@ -41,49 +41,49 @@ impl Plummer
 
         return Vector3{ x, y, z };
     }
-}
 
-impl Generator for Plummer
-{
-    fn generate(&self) -> Result<ParticleSet, &'static str> 
+    fn generate_positions(&self) -> Vec::<Vector3>
     {
         let number_of_divisions: usize = (self.n as f32).powf(1.0 / 3.0) as usize;
-        let mut h = vec![0.0_f64; number_of_divisions];
-        let max_val = 10.0 * self.plummer_radius;
-        let division = 1.0 / (number_of_divisions as f64) * max_val;
+        let max_r = 10.0 * self.plummer_radius;
+        let division = 1.0 / (number_of_divisions as f64) * max_r;
+        let mut descrete_rhos = vec![0.0_f64; number_of_divisions];
 
         for i in 0..number_of_divisions
         {
-            h[i] = self.rho(division * (i as f64));
+            descrete_rhos[i] = self.rho(division * (i as f64));
         }
 
-        let mut p = vec![0.0_f64; number_of_divisions + 1];
-        let sum: f64 = h.iter().sum();
-        
+        let sum: f64 = descrete_rhos.iter().sum();
+        let mut probabilities = vec![0.0_f64; number_of_divisions + 1];
+
         for i in 0..number_of_divisions
         {
-            p[i] = h[i] / sum;
+            probabilities[i] = descrete_rhos[i] / sum;
         }
-
-        p[number_of_divisions] = 1.0;
 
         let mut generator = StdRng::from_entropy();
         let mut number_of_particles_in_cell = vec![0; number_of_divisions];
 
         for _ in 0..self.n
-        {
-            let num: f64 = generator.sample(Standard);
+        {    
+            let mut num: f64 = generator.sample(Standard);
 
             for j in 0..number_of_divisions
             {
-                if p[j] < num && p[j + 1] > num
+                if num > probabilities[j]
+                {
+                    num -= probabilities[j];
+                }
+                else
                 {
                     number_of_particles_in_cell[j] += 1;
+                    break;
                 }
             }
         }
 
-        let mut output = ParticleSet::new()?;
+        let mut output = Vec::<Vector3>::new();
 
         for i in 0..number_of_divisions
         {
@@ -102,14 +102,44 @@ impl Generator for Plummer
                 let theta = theta * PI;
                 let curr_r = division * (i as f64) + division * (j as f64) / (curr as f64);
 
-                output.particles.push(
-                    Particle::new(
-                        Self::spherical_to_cartesian(curr_r, phi, theta), 
-                        Vector3::null_vector(), 
-                        self.m0 / (self.n as f64)
-                    )?
-                );
+                output.push(Self::spherical_to_cartesian(curr_r, phi, theta));
             }
+        }
+
+        return output;
+    }
+
+    fn generate_velocities(&self) -> Vec::<Vector3>
+    {
+        let output = vec![Vector3::null_vector(); self.n];
+
+        return output;
+    }
+}
+
+impl Generator for Plummer
+{
+    fn generate(&self) -> Result<ParticleSet, &'static str> 
+    {
+        let mut output = ParticleSet::new()?;
+        let positions = self.generate_positions();
+        let velocities = self.generate_velocities();
+        let masses = vec![self.m0 / (self.n as f64); self.n];
+
+        if (positions.len() != velocities.len()) || (positions.len() != masses.len())
+        {
+            return Err("generation of velocities, positions or masses went wrong");
+        }
+
+        for i in 0..positions.len()
+        {
+            output.particles.push(
+                Particle::new(
+                    positions[i], 
+                    velocities[i], 
+                    masses[i]
+                )?
+            );
         }
 
         return Ok(output);
